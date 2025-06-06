e3685721a1461d3ba8b8170e5b143f33eecc722e
Athira-Antony <athirathereseantony@gamil.com>
2025-06-06 20:36:29 +0530

    notification

---
+++ backend/models/notification.js
@@ -0,0 +1,83 @@
+import pool from "../db.js";
+
+export const insertPostLike = async (post_id, user_id) => {
+    await pool.query(
+        `INSERT INTO post_likes (post_id, liked_by)
+        VALUES ($1,$2)
+        ON CONFLICT (post_id, liked_by) DO NOTHING`,
+        [post_id,user_id]
+    );
+};
+
+export const getPostOwner = async(post_id) => {
+    const {rows} = await pool.query(
+        `SELECT p.user_id AS owner_id, u.firstname AS owner_name
+        FROM posts p
+        JOIN users u ON p.user_id = u.id
+        WHERE p.id = $1`,
+        [post_id]
+    );
+    return rows[0];
+}
+
+export const getUserName = async(user_id) => {
+    const {rows} = await pool.query(
+        `SELECT firstname FROM users WHERE id = $1`,
+        [user_id]
+    );
+    return rows[0];
+}
+
+export const insertNotification = async({sender_id, receiver_id, content, post_id, type}) => {
+    const {rows} = await pool.query(
+        `INSERT INTO notifications (sender_id, receiver_id,content,post_id,type)
+        VALUES ($1,$2,$3,$4,$5)
+        RETURNING *`,
+        [sender_id,receiver_id,content,post_id,type]
+    );
+    return rows[0];
+}
+
+export const insertComment = async ({post_id, user_id, content}) => {
+    const {rows} = await pool.query(
+        `INSERT INTO comments (post_id,user_id,content)
+        VALUES ($1,$2,$3)
+        RETURNING *`,
+        [post_id,user_id,content]
+    );
+   return rows[0];
+}
+
+
+export const reqExists = async (requestor_id, acceptor_id) => {
+    const {rows} = await pool.query(
+        `SELECT 1 FROM requests 
+        WHERE requestor_id = $1 AND acceptor_id = $2 AND status='pending'`,
+        [requestor_id,acceptor_id]
+    );
+    return rows.length > 0;
+}
+
+export const alreadyFollowing = async (follower_id, following_id) => {
+    const { rows } = await pool.query(
+      `SELECT 1 FROM friends WHERE follower_id = $1 AND following_id = $2`,
+      [follower_id, following_id]
+    );
+    return rows.length > 0;
+  };
+  
+
+  export const insertConnectionNotification = async ({
+    sender_id,
+    receiver_id,
+    content,
+    type
+  }) => {
+    const { rows } = await pool.query(
+      `INSERT INTO notifications (sender_id, receiver_id, content, type)
+       VALUES ($1, $2, $3, $4)
+       RETURNING *`,
+      [sender_id, receiver_id, content,type]
+    );
+    return rows[0];
+  };
--- backend/socketControllers/notificationController.js
+++ backend/socketControllers/notificationController.js
@@ -1,16 +1,81 @@
-export const likePost = async (user_id, post_id) => {
+import { alreadyFollowing, getPostOwner, getUserName, insertComment, insertConnectionNotification, insertNotification, insertPostLike, reqExists } from "../models/notification";
+
+export const likePost = async (user_id, post_id,io) => {
   // insert to post like table
   // add to notifications
   // emit post_liked signal to send a notification to the post creator
+
+  try{
+    await insertPostLike(post_id,user_id);
+
+    const receiver = await getPostOwner(post_id);
+
+    if(!receiver || receiver.owner_id === user_id)
+      return;
+
+    const sender = await getUserName(user_id);
+    const content = `${sender.firstname} liked your post`;
+
+    const notification = await insertNotification({sender_id:user_id, receiver_id:receiver.owner_id, content, post_id,type:"Like"});
+
+    io.to(`user_${receiver}.owner_id`).emit("post_liked",{notification});
+
+    console.log(`Notification sent to user ${receiver.owner_id}`);
+  }catch(err){
+      console.error("Error in likePost :", err);
+  }
+
 };
 
-export const commentPost = async (user_id, comment) => {
+export const commentPost = async (user_id, comment,io) => {
   // insert into comments table
   // add to notifications
   // emit post_commented signal to send a notification to the post creator
+
+  try{
+    const {post_id, content} = comment;
+    
+    const insertCmnt = await insertComment({post_id,user_id,content});
+
+    const owner = await getPostOwner(post_id);
+    if (!owner || owner.owner_id === user_id)
+      return;
+    
+    const sender = await getUserName(user_id);
+    const cmntContent = `${sender.firstname} commented on your post`;
+
+    const notification = await insertNotification({sender_id:user_id, receiver_id:owner.owner_id, content:cmntContent, post_id, type:"Comment"});
+
+    io.to(`user_${owner}.owner_id`).emit("post_commented",{notification, comment:insertCmnt});
+    console.log("comment notification sent successfully");
+
+  }catch(err){
+    console.error("Error in commentPost : ",err);
+  }
 };
 
-export const sendConnectionRequest = async (sender_id, receiverId) => {
+export const sendConnectionRequest = async (sender_id, receiver_id,io) => {
   // add to notifications
   // emit reveive_connection_request to send a notification to the receiver
+
+  try{
+    const alreadyFrnds = await alreadyFollowing(sender_id,receiver_id);
+    const alreadyReq = await reqExists(sender_id,receiver_id);
+    if(alreadyFrnds || alreadyReq)
+    {
+      console.log("Can't send request again");
+      return;
+    }
+
+    const sender = await getUserName(user_id);
+    const reqContent = `${sender.firstname} sent a friend request`;
+
+    const notification = await insertConnectionNotification({sender_id, receiver_id, content:reqContent, type:"Friend-request"});
+
+    io.to(`user_${owner}.owner_id`).emit("requested_friend_connection",{notification});
+    console.log("Friend request sent successfully");
+
+  }catch(err){
+    console.error("Error in sendConnectionRequest : ",err);
+  }
 };
