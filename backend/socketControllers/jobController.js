a6f889ca857e03483878cfaa9e29213a6363a82d
Alvin0305 <alvinanildas@gmail.com>
2025-06-06 20:27:20 +0530

    socket change

--- backend/socketControllers/jobController.js
+++ backend/socketControllers/jobController.js
@@ -1,9 +1,30 @@
 export const createJob = async (employer_id, job) => {
   // insert into the job table
+  // find all the employees who satisfies the job requirements
+  // loop through the employee ids
+  // ---- example -----
+  /*
+  
+  employeeIds.forEach((employeeId) => {
+    io.to(`user_${employeeId}`).emit("job_created", {
+      job, employerId, ... other things which may be sent to the employee
+    });
+  })
+
+  */
   // emit job_created signal
 };
 
 export const replyJob = async (employee_id, job_id) => {
   // insert into job reply
+  // send a replyd_to_job signal to the employer.
+  // get the employer id from the job using the job_id
+  // and send the signal to him
+  // ---- example ------
+  /*
+  io.to(`user_${employerId}`).emit("reply_to_job", {
+    employeeId, ... other things which may be sent to the employer
+  })
+  */
   // emit replyd_to_job signal
 };
--- backend/socketControllers/notificationController.js
+++ backend/socketControllers/notificationController.js
@@ -2,7 +2,9 @@ export const likePost = async (user_id, post_id) => {
   // insert to post like table
   // add to notifications
   // emit post_liked signal to send a notification to the post creator
-  // i.e., io.to(`user_${post_creater_id}`).emit("post_liked");
+  // i.e., io.to(`user_${post_creater_id}`).emit("post_liked", {
+  //  user_id, post_details, ... other fields that might be needed in front end to show in the notification
+  // });
   //  ---- take the post creater id from the post using post id or take it as parameter.
 };
 
@@ -10,12 +12,16 @@ export const commentPost = async (user_id, comment) => {
   // insert into comments table
   // add to notifications
   // emit post_commented signal to send a notification to the post creator
-  // i.e., io.to(`user_${post_creater_id}`).emit("post_commented");
+  // i.e., io.to(`user_${post_creater_id}`).emit("post_commented", {
+  //  user_id, post_details, ... other fields that might be needed in front end to show in the notification
+  // });
   //  ---- take the post creater id from the post using post id or take it as parameter.
 };
 
 export const sendConnectionRequest = async (sender_id, receiverId) => {
   // add to notifications
   // emit reveive_connection_request to send a notification to the receiver
-  // i.e., io.to(`user_${receiverId}`).emit("receive_connection");
+  // i.e., io.to(`user_${receiverId}`).emit("receive_connection", {
+  //  sender_id, ... other fields that might be needed in front end to show in the notification
+  // });
 };
