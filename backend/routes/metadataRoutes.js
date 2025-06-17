9274f1900664c3c02af91b0bba20998097cc32fb
Athira Antony <147913045+Athira-Antony@users.noreply.github.com>
2025-06-17 10:17:09 +0530

    Update metadataRoutes.js

--- backend/routes/metadataRoutes.js
+++ backend/routes/metadataRoutes.js
@@ -36,4 +36,14 @@ router.get("/codinglanguage/:id",protect, getCodingLanguagesOfUser);
 router.get("/language/:id",protect, getLanguagesOfUser);
 router.get("/qualification/:id",protect, getQualificationsOfUser);
 
+router.post("/decription/:id",protect, createDescription);
+router.get("/description/:id",protect,getDescription);
+
+router.post("/workexperience/:id",protect,createWorkExperienceFunction); // only for employee
+router.get("/workexperience/:id",protect,getWorkExperienceFunction);
+// router.put("/workexperience/:id",protect,)
+
+router.post("/jobdetails/:id",protect,createJobDetails);   // for employer -> only company, designation, location 
+router.get("/jobdetails/:id",protect,getJobDetails);
+
 export default router;
